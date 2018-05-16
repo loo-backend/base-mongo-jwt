4dad49098cd00736ac8ed67b42122eca07db1e53
williamsduarte <wsduarte@outlook.com>
2018-05-16 09:48:33 -0400

    Middleware JWT

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -57,6 +57,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'jwt.auth' => \Helpdesk\Http\Middleware\AuthJWT::class,
+        'jwt.auth' => \App\Http\Middleware\AuthJWT::class,
     ];
 }
